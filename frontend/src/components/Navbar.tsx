4b9795a5d5d3248ada2e65fe7fcf4813d2ae9e87
Sahil Vaingankar <sahilvaingankar21@gmail.com>
2025-07-12 20:49:08 +0530

    Navbar component created

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,29 @@
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { FaSearch, FaShoppingCart } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <div
+      className="flex items-center gap-1 h-12 w-full p-2 border-b-3 shadow-gray-400 shadow-md"
+      //   style={{ boxShadow: "0 3px 6px 0 rgba(0,0,0,0.3)" }}
+    >
+      <h1 className="text-16 font-bold">LOGO</h1>
+      <div className="flex gap-1 items-center h-[30px] w-[166px] rounded-[25px] border-1">
+        <div className="flex items-center justify-center h-[30px] w-8 rounded-l-[25px] border-r-1">
+          <FaSearch />
+        </div>
+        <input
+          type="text"
+          placeholder="Search..."
+          className="placeholder:text-[#9D9A9A] w-30"
+        />
+      </div>
+      <AiOutlineShoppingCart className="h-[25px] w-[25px]" />
+      <button className="h-[28px] w-[55px] border-1 rounded-[25px] text-sm">
+        Login
+      </button>
+    </div>
+  );
+};
+
+export default Navbar;
