9a525ad707487a9fd15b41b536717e65cba5e1a4
Sahil Vaingankar <sahilvaingankar21@gmail.com>
2025-07-15 13:28:37 +0530

    initial Sidebar UI for Home page is completed

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,17 +1,103 @@
+import { AiOutlineClose } from "react-icons/ai";
+
 function Sidebar({ isClicked }: { isClicked: boolean }) {
   return (
     <div
-      className={`sm:absolute fixed top-10 sm:top-0 left-0 flex flex-col h-[100svh] w-0 overflow-hidden ${
-        isClicked ? "w-50" : "w-0 sm:w-50"
-      } bg-white`}>
-      <ul>
-        <h1>Categories</h1>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-      </ul>
+      className={`sm:absolute fixed top-5 sm:top-0 left-0 flex flex-col justify-around h-[100svh] w-0 sm:px-2 py-6.5 overflow-hidden ${
+        isClicked ? "w-[250px] px-2" : "w-[250px] sm:w-[250px]"
+      } bg-[#E8E5E5]`}>
+      <div>
+        <div className="flex justify-between text-center items-center h-15">
+          <h2 className="ml-1 font-semibold text-[36px]">Categories</h2>
+          <button className=" sm:hidden font-semibold text-[36px] mt-1 p-0.5 border-2 border-[#E8E5E5] hover:border-purple-500">
+            <AiOutlineClose className=" sm:hidden h-10 w-10" />
+          </button>
+        </div>
+        <ul className="mt-4 space-y-4">
+          <li>
+            <label className="cursor-pointer">
+              <input
+                type="radio"
+                name="category"
+                className="mr-2 w-[16px] h[16px] cursor-pointer"
+              />
+              electronics
+            </label>
+          </li>
+          <li>
+            <label className="cursor-pointer">
+              <input
+                type="radio"
+                name="category"
+                className="mr-2 w-[16px] h[16px] cursor-pointer"
+              />
+              electronics
+            </label>
+          </li>
+          <li>
+            <label className="cursor-pointer">
+              <input
+                type="radio"
+                name="category"
+                className="mr-2 w-[16px] h[16px] cursor-pointer"
+              />
+              electronics
+            </label>
+          </li>
+          <li>
+            <label className="cursor-pointer">
+              <input
+                type="radio"
+                name="category"
+                className="mr-2 w-[16px] h[16px] cursor-pointer"
+              />
+              electronics
+            </label>
+          </li>
+          <li>
+            <label className="cursor-pointer">
+              <input
+                type="radio"
+                name="category"
+                className="mr-2 w-[16px] h[16px] cursor-pointer"
+              />
+              electronics
+            </label>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h2 className="ml-1 font-semibold text-[36px]">Price range</h2>
+        <div className="flex justify-between">
+          <label className="text-center">
+            <h3>min</h3>
+            <input
+              type="text"
+              placeholder="- -"
+              className="bg-white mx-1 h-9 w-[109px] border text-center placeholder:text-2xl outline-none"
+            />
+          </label>
+          <label className="text-center">
+            <h3>max</h3>
+            <input
+              type="text"
+              placeholder="- -"
+              className="bg-white mx-1 h-9 w-[109px] border text-center placeholder:text-2xl outline-none"
+            />
+          </label>
+        </div>
+      </div>
+      <div className="flex justify-between sm:hidden">
+        <button className="bg-black text-white px-8 py-2 rounded-[25px] border-3 border-white">
+          Cancel
+        </button>
+        <button className="bg-black text-white px-8 py-2 rounded-[25px] border-3 border-white">
+          Apply
+        </button>
+      </div>
+      <button className="hidden sm:block bg-black text-white px-8 py-2 rounded-[25px] border-3 border-white">
+        Reset
+      </button>
     </div>
   );
 }
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -11,58 +11,63 @@ const Home = () => {
   const [isClicked, setIsClicked] = useState<boolean>(false);
   return (
     <>
-      <Navbar />
+      <header>
+        <Navbar />
+      </header>
 
-      <main className="flex overflow-y-auto mt-10">
+      <div className="flex overflow-y-auto mt-5">
         <aside
           className={`absolute sm:relative sm:flex sm:flex-col bg-black min-h-[100svh] z-20 w-0 ${
-            isClicked ? "w-full sm:w-50 " : "w-0"
+            isClicked ? "w-full sm:w-[250px]" : "w-0"
           } overflow-hidden transition-all duration-700 md:border-r-2`}>
           <Sidebar isClicked={isClicked} />
         </aside>
-        <section className="p-2 flex flex-col">
-          <h2 className="mt-4.75 text-[20px] font-medium">Recommended</h2>
-          <section className="mt-2 flex gap-2 flex-wrap">
-            {recommendedBtns.map((btn: string, i: number) => (
+
+        <main className=" mt-10">
+          <section className="p-2 flex flex-col">
+            <h2 className="mt-4.75 text-[20px] font-medium">Recommended</h2>
+            <section className="mt-2 flex gap-2 flex-wrap">
+              {recommendedBtns.map((btn: string, i: number) => (
+                <button
+                  key={i}
+                  onClick={() => {
+                    selectedRecommendation === btn
+                      ? setselectedRecommendation("")
+                      : setselectedRecommendation(btn);
+                  }}
+                  className={`${
+                    selectedRecommendation === btn
+                      ? "bg-[#e7e7e7] hover:bg-[#e7e7e7]"
+                      : "hover:bg-[#F4F4F4]"
+                  } border py-1 px-2 rounded-[5px] cursor-pointer`}>
+                  {btn}
+                </button>
+              ))}
+            </section>
+            <section className="flex justify-between w-full mt-6">
               <button
-                key={i}
                 onClick={() => {
-                  selectedRecommendation === btn
-                    ? setselectedRecommendation("")
-                    : setselectedRecommendation(btn);
+                  setIsClicked(!isClicked);
                 }}
-                className={`${
-                  selectedRecommendation === btn
-                    ? "bg-[#e7e7e7] hover:bg-[#e7e7e7]"
-                    : "hover:bg-[#F4F4F4]"
-                } border py-1 px-2 rounded-[5px] cursor-pointer`}>
-                {btn}
+                className="py-1 px-4 rounded-[50px] border">
+                <FiFilter className="inline mb-1" /> Filter
               </button>
-            ))}
+              <SortBtn />
+            </section>
+            <section className="grid grid-cols-2 gap-5 mt-5">
+              <div className="bg-black h-30"></div>
+              <div className="bg-black h-30"></div>
+              <div className="bg-black h-30"></div>
+              <div className="bg-black h-30"></div>
+              <div className="bg-black h-30"></div>
+              <div className="bg-black h-30"></div>
+              <div className="bg-black h-30"></div>
+              <div className="bg-black h-30"></div>
+              <div className="bg-black h-30"></div>
+            </section>
           </section>
-          <section className="flex justify-between w-full mt-6">
-            <button
-              onClick={() => {
-                setIsClicked(!isClicked);
-              }}
-              className="py-1 px-4 rounded-[50px] border">
-              <FiFilter className="inline mb-1" /> Filter
-            </button>
-            <SortBtn />
-          </section>
-          <section className="grid grid-cols-2 gap-5 mt-5">
-            <div className="bg-black h-30"></div>
-            <div className="bg-black h-30"></div>
-            <div className="bg-black h-30"></div>
-            <div className="bg-black h-30"></div>
-            <div className="bg-black h-30"></div>
-            <div className="bg-black h-30"></div>
-            <div className="bg-black h-30"></div>
-            <div className="bg-black h-30"></div>
-            <div className="bg-black h-30"></div>
-          </section>
-        </section>
-      </main>
+        </main>
+      </div>
     </>
   );
 };
