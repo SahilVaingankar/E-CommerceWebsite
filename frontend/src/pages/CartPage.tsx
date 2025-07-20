80e36bbabac50bb9c4b0260f0a25e0d1857bb476
Sahil Vaingankar <sahilvaingankar21@gmail.com>
2025-07-20 13:04:53 +0530

    more UI fixes in CartPage

--- frontend/src/pages/CartPage.tsx
+++ frontend/src/pages/CartPage.tsx
@@ -41,7 +41,7 @@ const CartPage = () => {
 
   return (
     <div className="flex justify-center items-center h-screen w-full p-2 bg-gradient-to-br from-indigo-500 to-lime-500">
-      <div className="flex flex-col gap-1 mt-15 bg-white w-full p-2 min-h-100 overflow-y-auto shadow-2xl">
+      <div className="flex flex-col gap-1 mt-15 bg-white w-full max-w-300 p-2 md:p-4 min-h-100 overflow-y-auto shadow-2xl">
         <div className="flex justify-between">
           {" "}
           <h1>Shopping Cart</h1>
@@ -53,37 +53,39 @@ const CartPage = () => {
             Remove all
           </p>
         </div>
-        <div className="flex flex-col justify-center p-2 gap-2 grow h-full border overflow-y-auto">
+        <div className="flex flex-col p-2 gap-2 grow h-10 border overflow-y-auto">
           {cartItems.length > 0 ? (
             cartItems.map((item) => (
-              <div className="flex justify-between items-center gap-2">
+              <div className="grid grid-cols-4 gap-2">
                 <img src={item.images[0]} alt="" className="h-15 w-auto" />
                 <h2 className="text-xs font-black">{item.title}</h2>
                 <div className="flex gap-1">
                   <button
-                    className="bg-gray-300 rounded-full h-7 w-7 pb-1"
+                    className="bg-gray-300 rounded-full h-7 w-7 pb-1 disabled:bg-gray-100 disabled:cursor-not-allowed"
                     onClick={() => {
                       decrement(item.title);
-                    }}>
+                    }}
+                    disabled={item.quantity! === 1}>
                     -
                   </button>
                   <p>{item.quantity}</p>
                   <button
                     className="bg-gray-300 rounded-full h-7 w-7 pb-1"
-                    onClick={() => increment(item.title)}>
+                    onClick={() => increment(item.title)}
+                    disabled={item.quantity! > 30}>
                     +
                   </button>
                 </div>
-                <div className="flex flex-col text-center">
-                  <p className="text-sm font-semibold">
-                    ${item.price * (item.quantity || 1)}
-                  </p>
-                  <div
-                    className="text-red-500 border-b text-xs"
+                {/* <div className=""> */}
+                <p className="flex flex-col text-center text-sm font-semibold">
+                  ${item.price * (item.quantity || 1)}
+                  <span
+                    className="m-auto text-red-500 border-b text-xs"
                     onClick={() => remove(item.title)}>
                     Remove
-                  </div>
-                </div>
+                  </span>
+                </p>
+                {/* </div> */}
               </div>
             ))
           ) : (
