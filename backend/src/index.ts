7628c307897b097e64da2de7720cadded8f6fe9f
Sahil Vaingankar <sahilvaingankar21@gmail.com>
2025-08-24 16:07:48 +0530

    fixed typo in URL

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,7 @@ const port = process.env.PORT || 50000;
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "http://e-commercewebsite-production-37d2.up.railway.app",
+  "https://e-commercewebsite-production-37d2.up.railway.app",
 ];
 
 app.use(express.json());
